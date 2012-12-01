e6857858685c2a96de32b826b1caabbe9ba01239
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2012-12-01 19:16:34 -0200

    Js datepick- Now is possible to pick the date using a nice calendar. fix #6

--- rmr/apps/books/models.py
+++ rmr/apps/books/models.py
@@ -51,7 +51,7 @@ class Book(Abs_Named_Model, Abs_UserConected_Model):
     #purchase data
     purchase_store = models.ForeignKey('stores.Store',related_name='books',null=True,blank=True)
     purchased       = models.BooleanField(_("Purchased?"),default=False)
-    purchase_value = models.DecimalField(_("Purchase Value"),max_digits=10, decimal_places=2,default=Decimal("0"), null=True, blank=True)
+    purchase_value = models.DecimalField(_("Purchase Value"),max_digits=10, decimal_places=2, null=True, blank=True)
     purchase_date =  models.DateField(_("Purchase Date"),  null=True, blank=True)#, default=datetime.date.today,)
     
     
@@ -72,7 +72,7 @@ class Book(Abs_Named_Model, Abs_UserConected_Model):
         
     def save(self, *args, **kwargs):
         """
-        If the task is assigned to someone, then add this member to the project members
+        if has any purchased information, then mark as purchased
         """
         #if has a purchase value or date then mark as purchased
         if self.purchase_value or self.purchase_date or self.purchase_store:
